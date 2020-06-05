d05ffb3d3d7eea22bf2e1f71e6a60ac182ea4e22
EmmanuelSalinas <salinas.e9810@gmail.com>
2020-06-05 01:15:06 -0500

    se terminaron de agregar rutas para la ruta usuario

--- src/index.js
+++ src/index.js
@@ -6,12 +6,21 @@ const user = require('./routes/user')
 
 //middleware
 app.use(morgan('dev'));
+    //para usar el body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/" , (req,res,next)=>{
-    res.send("Bienvenido al servidor");
+    res.status(200).json({code:1,message:"Bienvenido al Servidor"})
 });
 
 app.use("/user",user)
+
+
+app.use((req,res,next)=>{
+    return res.status(404).json({code:404, message:"URL no encontrada"});
+});
 
 app.listen(process.env.PORT ||3000, () => {
     console.log("Server is running... ;)")
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,32 +3,79 @@ const user = express.Router();
 const jwt= require('jsonwebtoken');
 const db = require('../config/database')
 
-
-user.get('/:id([0-9]{1,3})', async(req,res,next)=>{
-    const id = req.params.id;
-    const user = await db.query(`SELECT * FROM user WHERE user_id ='${id}';`);
+//ruta para agregar un nuevo usuario a la db
+user.post("/", async(req,res,next) => {
+    const {user_name,user_lastName,user_mail,user_position,
+        user_permission} = req.body;
     
-    console.log(id);
+    if(user_name && user_lastName && user_mail && user_position &&
+    user_permission){
+        
+        let query = "INSERT INTO user(user_name, user_lastName,"
+        
+        query += " user_mail, user_position, user_permission)"
+        query += ` VALUES ('${user_name}', '${user_lastName}',
+        '${user_mail}', '${user_position}', '${user_permission}');`;
+        
+        const usuario = await db.query(query);
 
-    if(id >= 1 && id <= 100){
-        if(user.length == 1){    
-            console.log("esta dentro1")
-            return res.status(200).json({code:200 , message:user})
+        if(usuario.affectedRows == 1){
+            return res.status(201).json({code:201, message:"Usuario insertado Correctamente"});
         }
-        else{
-            return res.status(404).json({code: 404, message: "usuario no encontrado"});
+        return res.status(500).json({code:500, message:"oh oh Ocurrio un error"});
+    }
+
+    return res.status(500).json({code:500, message:"Campos incompletos"});
+    
+});
+//ruta para eliminar usuario de la db
+user.delete("/:id([0-9]{1,3})", async(req,res,next)=>{
+    const query = `DELETE FROM user WHERE user_id = ${req.params.id}`;
+    const user = await db.query(query);
+
+    if(user.affectedRows == 1){
+        return res.status(200).json({code:200, message:"Usuario Eliminado Correctamente"});
+    }
+    return res.status(404).json({code:404, message:"Usuario No Encontrado"});
+});
+//ruta para modificar por completo al usuario
+user.put("/:id([0-9]{1,3})" , async(req,res,next) =>{
+    const {user_name,user_lastName,user_mail,user_position,
+    user_permission} = req.body;
+
+    if(user_name && user_lastName && user_mail && user_position &&
+    user_permission){
+        let query = `UPDATE user SET user_name = '${user_name}',  user_lastName = '${user_lastName}', user_mail = '${user_mail}',`;
+        query += ` user_position = '${user_position}', user_permission = '${user_permission}'  WHERE user_id = ${req.params.id};`;
+        
+        const user = await db.query(query);
+
+        if(user.affectedRows == 1){
+            return res.status(200).json({code:200, message: "Usuario Actualizado Correctamente"});
         }
+        return res.status(500).json({code:500, message:"oh ooh ocurrio un error"});
     }
-    else{
-        return res.status(404).json({code: 404, message: "id invalido"});
-    }
+    return res.status(500).json({code:500, message:"Campos incompletosm"})
 });
 
 
 user.get('/' , async(req,res,next)=>{
     const user = await db.query("SELECT * FROM user");
     
-    return res.status(200).json({code:200, message:user});
+    return res.status(200).json({code:1, message:user});
+});
+
+user.get('/:id([0-9]{1,3})', async(req,res,next)=>{
+    const id = req.params.id;
+    
+    if(id >= 1 && id <= 100){
+        const user = await db.query(`SELECT * FROM user WHERE user_id ='${id}';`);
+        
+        return res.status(200).json({code:1 , message:user})
+    }
+    
+    return res.status(404).json({code: 404, message: "usuario no encontrado"});
+    
 });
 
 
